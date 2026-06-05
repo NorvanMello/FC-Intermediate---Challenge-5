af778e16c20d9276da2f96108e66df72471dc249
Norvan <110420594+NorvanMello@users.noreply.github.com>
2026-06-05 12:49:21 -0300

    Update formatDate.js

--- formatDate.js
+++ formatDate.js
@@ -11,6 +11,20 @@ export function formatWeatherDate(isoDate) {
     return parsedDate.toLocaleString("en-Us", options);
 }
 
+export function longWeekDay(isoDate) {
+    const year = isoDate.slice(0,4)
+    const month = isoDate.slice(5,7)
+    const day = isoDate.slice(8,10)
+
+    const parsedDate = new Date(year, month-1, day);
+
+    const options = {
+        weekday: "long",
+    }
+
+    return parsedDate.toLocaleString("en-Us", options);
+}
+
 export function shortWeekDay(isoDate) {
     const year = isoDate.slice(0,4)
     const month = isoDate.slice(5,7)
