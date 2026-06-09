d13d4d3f00f2dd942661cb0a856a2409f5d612a4
Norvan <110420594+NorvanMello@users.noreply.github.com>
2026-06-08 21:49:07 -0300

    Update weatherAPI.js

--- weatherAPI.js
+++ weatherAPI.js
@@ -6,15 +6,14 @@ export async function getPlace(cityName) {
     return data.results[0];
 }
 
-export async function getWeatherInfo(cityName) {
+export async function getWeatherInfo(cityName, temperatureUnit = "", windUnit = "", precipitationUnit = "") {
     const place = await getPlace(cityName);
 
-    const url = `https://api.open-meteo.com/v1/forecast?latitude=${place.latitude}&longitude=${place.longitude}&daily=weather_code,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,weather_code&current=temperature_2m,relative_humidity_2m,apparent_temperature,weather_code,precipitation,wind_speed_10m&timezone=auto`
-
+    console.log(temperatureUnit)
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${place.latitude}&longitude=${place.longitude}&daily=weather_code,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,weather_code&current=temperature_2m,relative_humidity_2m,apparent_temperature,weather_code,precipitation,wind_speed_10m&timezone=auto${temperatureUnit}${windUnit}${precipitationUnit}`
+    console.log(url)
     const response = await fetch(url);
     const data = await response.json();
-
-    console.log(data)
 
     return {
         place,
