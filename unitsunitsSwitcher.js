d4c0bc4f0fce0985d4ebd5b7e47d5fd109839a71
Norvan <110420594+NorvanMello@users.noreply.github.com>
2026-06-08 21:48:35 -0300

    Update unitsunitsSwitcher.js

--- unitsunitsSwitcher.js
+++ unitsunitsSwitcher.js
@@ -19,30 +19,40 @@ function toggleUnitsContainer(unitsDropdown) {
     unitsDropdown.setAttribute("aria-hidden", "true");
 }
 
-async function imperialMetricSwticher(metricImperial, temperatureContainer) {
+async function imperialMetricSwticher(metricImperial, temperatureContainer, currentWeatherDetails) {
     if(metricImperial === "Switch to Metric") {
-       const weatherInfoMetric = await getWeatherInfo("patrocínio do muriaé", "&temperature_unit=fahrenheit");
+       const weatherInfoMetric = await getWeatherInfo("patrocínio do muriaé", "&temperature_unit=fahrenheit", "&wind_speed_unit=mph", "&precipitation_unit=inch");
 
         renderTemperature(temperatureContainer, weatherInfoMetric);
+        renderCurrentWeatherDetails(currentWeatherDetails, weatherInfoMetric)
     } else {
         const weatherInfoMetric = await getWeatherInfo("patrocínio do muriaé", "");
 
         renderTemperature(temperatureContainer, weatherInfoMetric);
+        renderCurrentWeatherDetails(currentWeatherDetails, weatherInfoMetric)
     }
 }
 
-export function unitSwticherEvent(settingsBtn,unitsDropdown, unit, temperatureContainer) {
+export function unitSwticherEvent(settingsBtn,unitsDropdown, unit, temperatureContainer, currentWeatherDetails) {
     settingsBtn.addEventListener("click", () => {
         toggleUnitsContainer(unitsDropdown);
     })
 
     unit.forEach(element => {
-        element.addEventListener("click", (e) => {
+        console.log(element.dataset.unit)
+        if(element.dataset.unit === "metricImpirial") {
+            element.addEventListener("click", (e) => {
             e.target.innerText === "Switch to Imperial" ?  e.target.innerText = "Switch to Metric" : e.target.innerText = "Switch to Imperial"
 
-            imperialMetricSwticher(e.target.innerText, temperatureContainer)
-        })
-    });
-    
+            imperialMetricSwticher(e.target.innerText, temperatureContainer, currentWeatherDetails)
+            })
+        }
 
+        if(element.dataset.unit === "temperature") {
+            element.addEventListener("click", (e) => {
+                
+            })
+        }
+
+    });
 }
